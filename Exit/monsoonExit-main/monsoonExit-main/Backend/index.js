b9d7cf912c6e022935fc85835889b56de6301301
athulya-dileep <dileepathulya623@gmail.com>
2025-02-03 16:15:52 +0530

    index.js

--- Exit/monsoonExit-main/monsoonExit-main/Backend/index.js
+++ Exit/monsoonExit-main/monsoonExit-main/Backend/index.js
@@ -1,32 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 require("./connection")
-var BlogModel = require("./model") 
+const BlogModel = require("./model");
 
 const app = express();
-var PORT = 3001;
+const PORT = 3001;
+
 app.use(express.json());
 app.use(cors());
 
+
 app.post("/add", async (req, res) => {
   try {
-      await BlogModel(req.body).save()
-      res.send({message:"data added"})
+    const newBlog = new BlogModel(req.body);
+    await newBlog.save();
+    res.send({ message: "Data added successfully" });
   } catch (error) {
-      console.log(error)
+    console.error(error);
+    res.status(500).send({ error: "Internal Server Error" });
   }
-})
+});
 
 app.get("/get", async (req, res) => {
   try {
-    let data = await BlogModel.find();
+    const data = await BlogModel.find();
     res.send(data);
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    res.status(500).send({ error: "Internal Server Error" });
   }
 });
 
 
 app.listen(PORT, () => {
-  console.log(`${PORT} is up and running`);
+  console.log(`Server is running on port ${PORT}`);
 });
